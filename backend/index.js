4df36842e49246e6bbc2bf0034092ca4c4bc900e
kienchidong <kienchidong@gmail.com>
2023-08-09 17:43:50 +0700

    up

---
+++ backend/index.js
@@ -0,0 +1,25 @@
+const express = require('express')
+const mysql = require('mysql')
+const cors = require('cors')
+const port = 3000;
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+app.listen(port, () => {
+    console.log("listening...");
+})
+
+
+const db = mysql.createConnection({
+    host:'localhost',
+    user:"root",
+    password:'',
+    database:'signup'
+})
+app.post('/signup', (req, res) => {
+    const { email, password } = req.body;
+
+    res.status(200).json({ message: 'Signup successful' });
+  });
+
